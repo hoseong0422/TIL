ffbb661f7e8d34599e073fce7a5e6e22e8f6d070
Hoseong <hoseong0422@gmail.com>
2024-05-06 15:27:04 +0900

    start study java

---
+++ java/HelloJava.java
@@ -0,0 +1,10 @@
+// 자바는 대소문자를 구분하므로 대소문자에 유의하자!
+
+public class HelloJava { //HelloJava 클래스의 범위 시작
+
+    // main 메소드 (프로그램의 시작점)
+    public static void main(String[] args) { //main() 메서드 범위 시작
+        System.out.println("Hello Java!"); // 값을 콘솔에 출력, 자바는 문자열을 ""로 감싸주어여 한다
+    } //main() 메서드 범위 끝
+
+} //HelloJava 클래스의 범위 끝
---
+++ java/HelloJava2.java
@@ -0,0 +1,9 @@
+public class HelloJava2 {
+    // main 메서드는 위에서부터 한 줄 씩 실행
+    public static void main(String[] args) {
+        System.out.println("Hello Java1");
+        System.out.println("Hello Java2");
+        System.out.println("Hello Java3");
+    }
+}
+
