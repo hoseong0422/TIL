e0920cc86f629fc3b18b0daf40ea5dbffc1e0f7b
Hoseong <hoseong0422@gmail.com>
2024-05-07 23:02:35 +0900

    주석 내용 추가

---
+++ java/CommentJava.java
@@ -0,0 +1,13 @@
+public class CommentJava {
+    public static void main(String[] args) { // psvm 입력 후 엔터입력하면 자동으로 입력해줌
+        // sout 엔터
+        System.out.println("hello java1"); // 한줄 주석은 이렇게
+        System.out.println("hello java2");
+
+        /*
+        여러줄 주석
+        System.out.println("hello java3");
+         */
+    }
+}
+
