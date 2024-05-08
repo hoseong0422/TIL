6b66c64e3aa01c6eaeb38e1ce0d37cf8cf2bc001
Hoseong <hoseong0422@gmail.com>
2024-05-08 22:19:51 +0900

    add varibale

---
+++ java/variable/Var1.java
@@ -0,0 +1,11 @@
+// 패키지는 폴더 개념으로 우선 이해하자
+package variable; //패키지에 들어가는 자바 파일 첫줄에는 본인이 소속된 패키지를 선언해야한다.
+
+public class Var1 {
+    public static void main(String[] args) {
+        // 변수를 사용하지 않으면 10을 20으로 출력하고 싶어지면 전부 수정 후 출력해야한다.
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+    }
+}
