90eab6410ac84a27b8ac7e238971ad882a449f17
Hoseong <hoseong0422@gmail.com>
2024-05-26 23:17:13 +0900

    add operator

---
+++ java/operator/Operator4.java
@@ -0,0 +1,10 @@
+package operator;
+
+public class Operator4 {
+    public static void main(String[] args) {
+        int sum3 = 2 * 2 + 3 * 3;
+        int sum4 = (2 * 2) + (3 * 3); // 코드 가독성을 높이기 위해 명시적으로 괄호를 사용하자!
+        System.out.println("sum3 = " + sum3);
+        System.out.println("sum4 = " + sum4);
+    }
+}
