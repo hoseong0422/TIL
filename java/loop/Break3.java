2e7fedbbb44b903aa60ac093cc9ca49d0e36af98
Hoseong <hoseong0422@gmail.com>
2024-07-13 21:19:07 +0900

    add for loop

---
+++ java/loop/Break3.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class Break3 {
+
+    public static void main(String[] args) {
+        // 1부터 시작해서 숫자를 계속 누적해서 더하다가 합계가 10보다 처음으로 큰 값은 언제일까?
+        int sum = 0;
+
+
+        for(int i=1; ; i++) {
+            sum += i;
+            if (sum > 10) {
+                System.out.println("합이 10보다 크면 종료: i = " + i + " sum = " + sum);
+                break;
+            }
+        }
+    }
+}
